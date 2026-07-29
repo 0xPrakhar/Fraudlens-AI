88327c8ec697b06784f85683e677383f4b5f8460
prakhar gupta <prakhargupta9083@gmail.com>
2026-07-29 14:43:46 +0530

    Complete backend updates

--- Backend/src/controllers/text.controller.js
+++ Backend/src/controllers/text.controller.js
@@ -1,4 +1,5 @@
 import { scanText } from "../services/text.service.js";
+import { saveScanHistory } from "../services/history.service.js";
 import { ApiError } from "../utiles/ApiError.js";
 import { ApiResponse } from "../utiles/ApiRespone.js";
 import { asyncHandler } from "../utiles/asyncHandler.js";
@@ -16,6 +17,14 @@ export const textScanner = asyncHandler(async (req, res) => {
     if (!response) {
         throw new ApiError(500, "Server Error");
     }
+
+    // Save Scan History
+    await saveScanHistory({
+        user: req.user._id,
+        scanType: "text",
+        input: text,
+        result: response,
+    });
 
     return res.status(200).json(
         new ApiResponse(
